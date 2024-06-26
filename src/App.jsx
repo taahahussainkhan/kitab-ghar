dda0f2c0efcabe8b73babf7e74a488a380df26ff
taahahussainkhan <taaha128@gmail.com>
2024-06-26 12:00:48 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    {/* Color setup */}
+    </>
+  )
+}
+
+export default App
